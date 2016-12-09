f52c53bc60ca30230fbfbd0070fffc3feb78e7b4
Bobby Brennan <bobby.brennan@gmail.com>
2016-12-09 13:00:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/static'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
 var index = fs.readFileSync(__dirname + '/static/index.html', 'utf8');
 app.get('*', function(req, res) {
   res.send(index);
