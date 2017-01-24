d0db312c5c9a2eb689a037dd27ae115368c5dc64
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-24 23:43:25 +0000

    add pug

--- app/components/navbar.ts
+++ app/components/navbar.ts
@@ -3,26 +3,7 @@ import {Router} from '@angular/router';
 
 @Component({
     selector: 'navbar',
-    template: `
-      <nav class="navbar navbar-default">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#NavbarCollapse" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <i class="fa fa-bars"></i>
-            </button>
-            <a class="navbar-brand" routerLink="/home">Website</a>
-          </div>
-          <div id="NavbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li [class.active]="router.isActive('/home')">
-                <a class="nav-link" routerLink="/home">Home</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      `,
+    template: require('!pug-loader!../views/navbar.pug'),
 })
 export class NavbarComponent {
   constructor(private router: Router) {}
